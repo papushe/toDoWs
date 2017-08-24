69d1386ecf7aa47dddcc627a2b019b2d14ee148b
Papushe <papushe9@gmail.com>
2017-08-24 09:32:11 +0300

    bug fixed

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers",
             "Origin, X-Requested-With, Content-Type, Accept");
+        res.header('Access-Control-Allow-Credentials', 'true');
         next();
     });
 
