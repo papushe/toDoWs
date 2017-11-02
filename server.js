b90528f3bd55410bec73adf346376cc0642bedd3
Papushe <papushe9@gmail.com>
2017-11-02 16:32:03 +0200

    remove node_modules

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ app.use(bodyParser.urlencoded({extended:true})); // parsing application/x-www-fo
 app.use('/', express.static('./public'));
 app.use('/assets', express.static(`${__dirname}/public`)); // public as assets
 app.use((req, res, next) => {
-    // res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Origin", "http://www.papushe.com");
+    res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Origin", "http://www.papushe.com");
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", `Content-Length, Authorization, Origin, X-Requested-With, Content-Type, Accept, application/json`);
